e5a3a419527c45e1aaa8f08d1c1742f1f8c05fbf
Lyx Huston <lyx@hustonfam.net>
2023-10-15 20:12:13 -0400

    boss entity framework improved

--- src/bosses.py
+++ src/bosses.py
@@ -19,6 +19,8 @@ entities file, but only for bosses
 """
 
 import entities
+import pygame
+import game_structures
 
 
 class Boss(entities.Entity):
@@ -28,5 +30,66 @@ class Boss(entities.Entity):
 
     alive = True
 
+    def __init__(self, img: pygame.Surface, rotation: int, pos: tuple[int, int]):
+        super().__init__(img, rotation, pos)
+        self.hit_track = []  # used to check if multiple body parts are being hit by the same thing in the same tick
+
     def player_entered(self):
         pass
+
+    def hit(self, damage: int, source):
+        if source not in self.hit_track:
+            self.health -= damage
+            self.hit_track.append(source)
+
+    def tick(self) -> bool:
+        self.hit_track.clear()
+        return self.health > 0
+
+
+class BodyPart(entities.Entity):
+    """
+    a body part of a boss
+    """
+
+    def __init__(self, img: pygame.Surface, rotation: int, pos: tuple[int, int], boss, damage: int = 5,
+                 collides: bool = True):
+        super().__init__(img, rotation, pos)
+        self.boss = boss
+        self.collides = collides
+        self.damage = damage
+
+    def tick(self) -> bool:
+        if not self.boss.alive:
+            return False
+        if not self.collides:
+            return True
+        if self.rect.colliderect():
+            if game_structures.deal_damage(self.damage, self):
+                game_structures.begin_shake(120, (200, 200), (21, 59))
+        return self.boss.alive
+
+    def hit(self, damage: int, source):
+        self.boss.hit(damage, source)
+
+    def draw(self):
+        """
+        draws boss body part, flashing based on if the boss is damaged.  Doesn't shake
+        """
+        if self.img is None:
+            return
+        if self.boss.flashing > 0:
+            img = pygame.Surface(self.img.get_rect().size, flags=pygame.SRCALPHA)
+            img.blit(self.img, (0, 0))
+            img.fill((255, 255, 255), special_flags=pygame.BLEND_ADD)
+            img.blit(self.img, (0, 0), None, pygame.BLEND_RGB_SUB)
+        else:
+            img = self.img
+        game_structures.SCREEN.blit(
+            img,
+            (
+                game_structures.to_screen_x(self.x) - img.get_width() // 2,
+                game_structures.to_screen_y(self.y) - img.get_height() // 2
+            )
+        )
+        return self.pos
