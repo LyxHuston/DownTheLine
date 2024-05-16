734113b9377fe0e853422150ab05a9f966b962df
Lyx Huston <lyx@hustonfam.net>
2024-05-16 12:50:18 -0400

    more command line arguments and better help messages

--- src/main.py
+++ src/main.py
@@ -19,6 +19,7 @@ main ran file for the game
 """
 import argparse
 import cProfile
+import ast
 
 import pygame
 
@@ -45,26 +46,71 @@ def run():
     pygame.quit()
 
 
+def int_tuple_from_string(num_args: int, argname: str, value_range: tuple[int | None, int | None] = (None, None)):
+    def inner(string: str):
+        try:
+            val = ast.literal_eval(string)
+            if not isinstance(val, tuple):
+                raise TypeError("Argument must be a tuple.")
+            if len(val) != num_args:
+                raise ValueError(f"Argument must be {num_args}(s) items long.")
+            for i, v in enumerate(val):
+                if not isinstance(v, int):
+                    raise TypeError(f"Item {i} is not an integer.")
+                if (value_range[0] is not None and v < value_range[0]) or (
+                        value_range[1] is not None and v > value_range[1]):
+                    raise ValueError(
+                        f"Item {i} is out of range: [{'' if value_range[0] is None else value_range[0]}:{'' if value_range[1] is None else value_range[1]}]"
+                    )
+            return val
+        except Exception:
+            raise ValueError(f"Command line argument {argname} was given invalid value: {ascii(string)}")
+    return inner
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="Down The Line")
-    parser.add_argument("mode", default="play", choices=["testing", "test_images", "play"], nargs="?")
-    parser.add_argument("-c", "--with_interactive_console", action="store_true")
-    parser.add_argument("-s", "--print_seed", action="store_true")
-    parser.add_argument("-p", "--profile", action="store_true")
+    parser.add_argument(
+        "mode", default="play", choices=["testing", "test_images", "play"], nargs="?",
+        help="The mode for the program to run in."
+    )
+    parser.add_argument(
+        "-c", "--with_interactive_console", action="store_true",
+        help="Start up an interactive console during runtime.  Please close before exit."
+    )
+    parser.add_argument(
+        "-s", "--print_seed", action="store_true",
+        help="Print the seed to console at the start of the run."
+    )
+    parser.add_argument(
+        "-p", "--profile", action="store_true",
+        help="Run the code profiler during runtime"
+    )
+    parser.add_argument(
+        "-d", "--dimensions",
+        type=int_tuple_from_string(2, "dimensions", (1, None)), default=None,
+        help="Set dimensions for the window.  If blank, is fullscreen.  Has priority over testing mode."
+    )
+    parser.add_argument(
+        "-b", "--backdrop",
+        type=int_tuple_from_string(3, "backdrop", (0, 255)), default=None,
+        help="Set the backdrop color for the game.  Useful, occasionally, for testing purposes."
+    )
     args = parser.parse_args()
 
     __run = True
 
+    dimens = (0, 0)
+    prompt = "run"
     if args.mode == "testing":
         backdrop = (128, 128, 128)
-        game_structures.SCREEN = pygame.display.set_mode((1000, 700))
+        dimens = (1000, 700)
     elif args.mode == "test_images":
         from data import images
-        images.test_images()
+        prompt = "images.test_images"
         __run = False
     elif args.mode == "play":
         backdrop = (0, 0, 0)
-        game_structures.SCREEN = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
     if __run:
         if args.print_seed:
@@ -81,13 +127,22 @@ if __name__ == "__main__":
             utility.make_async(code.InteractiveConsole().interact, daemon=True, log_errors=False)(
                 banner="Interactive console for Down The Line", exitmsg="Ending interactive console")
 
+        if args.dimensions is not None:
+            dimens = args.dimensions
+
+        if args.backdrop is not None:
+            backdrop = args.backdrop
+
+        game_structures.SCREEN = pygame.display.set_mode(dimens, pygame.FULLSCREEN if dimens == (0, 0) else 0)
+
         pygame.display.set_caption("Down the Line")
         pygame.display.set_icon(pygame.image.load("./resources/down_the_line.ico"))
         game_states.WIDTH, game_states.HEIGHT = game_structures.SCREEN.get_size()
         game_structures.determine_screen()
         # print(game_states.WIDTH, game_states.HEIGHT)
         game_states.CAMERA_THRESHOLDS = (
-        min(400, round(game_states.HEIGHT // 5)), min(400, round(game_states.HEIGHT // 5)))
+            min(400, round(game_states.HEIGHT // 5)), min(400, round(game_states.HEIGHT // 5))
+        )
 
         pygame.init()
         game_structures.init()
@@ -96,7 +151,10 @@ if __name__ == "__main__":
 
         game_structures.CUSTOM_EVENT_CATCHERS.append(lambda catch: game_states.PLACE.catcher(catch))
 
-        if args.profile:
-            cProfile.run("run()")
-        else:
-            run()
+    # prompt is never user defined
+    if not prompt.endswith("()"):
+        prompt += "()"
+    if args.profile:
+        cProfile.run(prompt)
+    else:
+        exec(prompt)
