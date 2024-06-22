7119e85d04b2d5b879d1853ffd73dd3b4c6ae74a
Lyx Huston <lyx@hustonfam.net>
2024-06-22 17:45:16 -0400

    add none check for reassigning targets

--- src/run_game/bosses.py
+++ src/run_game/bosses.py
@@ -165,7 +165,7 @@ class Serpent(Boss):
     def tick(self):
         if not self.state:
             return
-        if not self.target.alive:
+        if self.target is None or not self.target.alive:
             self.target = self.closest_enemy(self.area_length)
         last: Serpent.PathItem = self.next_path_item()
         part: Serpent.PathTracker
